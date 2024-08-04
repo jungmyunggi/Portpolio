e1c3906532ad1a62f4119d72380f9195f9454683
정명기 <jmk101711@naver.com>
2024-08-04 23:16:45 +0900

    feat: skill 컴포넌트

---
+++ portpolio/src/components/Skills.jsx
@@ -0,0 +1,24 @@
+import "../styles/Skills.scss";
+
+const Skills = () => {
+  return (
+    <section>
+      <div className="skill-container">
+        <div className="skill-element">
+          <p>Frontend</p>
+          <div className="basic">
+            <img src="../../public/assets/images/Icons/html.png" />
+            <img src="../../public/assets/images/Icons/css.png" />
+            <img src="../../public/assets/images/Icons/javascript.png" />
+          </div>
+        </div>
+        <div className="skill-element"></div>
+        <div className="skill-element"></div>
+        <div className="skill-element"></div>
+        <div className="skill-element"></div>
+      </div>
+    </section>
+  );
+};
+
+export default Skills;
